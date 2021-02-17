1e9e4b2ec5f8555fcee1f2b61f3405d468b8eb63
Andre <drefacey@gmail.com>
2021-02-16 19:49:33 -0500

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VueTextareaAutosize from "vue-textarea-autosize";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+
+Vue.use(VueTextareaAutosize);
+
+firebase.initializeApp({
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+});
+
+export const db = firebase.firestore();
+
+new Vue({
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
